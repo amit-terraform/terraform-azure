b66ef4a4144b18014f8b2ad47873a8c7ed763451
Amit Kumar <amitverma1150@gmail.com>
2021-08-27 19:34:42 +0530

    multiple providers

--- 08-Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
+++ 08-Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
@@ -17,7 +17,7 @@ resource "azurerm_storage_account" "mysa" {
   name = "mysa${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.myrg1.name
   location = azurerm_resource_group.myrg1.location
-  access_tier = "Standard"
+  account_tier = "Standard"
   account_replication_type = "GRS"
   account_encryption_source = "Microsoft.Storage"
 
