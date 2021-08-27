bf604a5a855503afad2fea61a5757c0cbf51f401
Amit Kumar <amitverma1150@gmail.com>
2021-08-27 19:45:50 +0530

    lock demo

--- 08-Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
+++ 08-Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
@@ -2,7 +2,7 @@
 
 resource "azurerm_resource_group" "myrg1" {
   name = "myrg-1"
-  location = "East-Us"
+  location = "East US"
 }
 
 # Resource-2: Random String 
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "mysa" {
   location = azurerm_resource_group.myrg1.location
   account_tier = "Standard"
   account_replication_type = "GRS"
-  account_encryption_source = "Microsoft.Storage"
+  # account_encryption_source = "Microsoft.Storage"
 
   tags = {
     environment = "staging"
