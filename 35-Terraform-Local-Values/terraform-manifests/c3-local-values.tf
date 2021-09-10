3d100e0bdf58f899db930914f527dd0712d284e5
Amit Kumar <amitverma1150@gmail.com>
2021-09-10 14:32:58 +0530

    local var

--- 35-Terraform-Local-Values/terraform-manifests/c3-local-values.tf
+++ 35-Terraform-Local-Values/terraform-manifests/c3-local-values.tf
@@ -2,12 +2,13 @@
 
 locals {
   # Use-case-1: Shorten the names for more readability
-  rg_name = "${var.business_unit}-${var.environment}-${var.resoure_group_name}"
-  vnet_name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
+  prefix = "${var.business_unit}-${var.environment}"
+  rg_name = "${local.prefix}-${var.resoure_group_name}"
+  vnet_name = "${local.prefix}-${var.virtual_network_name}"
 
   # Use-case-2: Common tags to be assigned to all resources
   service_name = "Demo Services"
-  owner = "Kalyan Reddy Daida"
+  owner = "Amit Kumar"
   common_tags = {
     Service = local.service_name
     Owner = local.owner
@@ -17,4 +18,4 @@ locals {
   # We will learn this when we are dealing with Dynamic Expressions
   # The expressions assigned to local value names can either be simple constants or can be more complex expressions that transform or combine values from elsewhere in the module.
  
-}  
+}
