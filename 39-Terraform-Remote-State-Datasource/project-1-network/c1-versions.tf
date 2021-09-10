7bf9628e1900bb8fef0ff4a889927ee544aa2cca
Amit Kumar <amitverma1150@gmail.com>
2021-09-10 19:49:11 +0530

    change rg name

--- 39-Terraform-Remote-State-Datasource/project-1-network/c1-versions.tf
+++ 39-Terraform-Remote-State-Datasource/project-1-network/c1-versions.tf
@@ -13,7 +13,7 @@ terraform {
   }
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
-    resource_group_name   = "terraform-storage-rg"
+    resource_group_name   = "tfamitstate05"
     storage_account_name  = "terraformstate201"
     container_name        = "tfstatefiles"
     key                   = "network-terraform.tfstate"
